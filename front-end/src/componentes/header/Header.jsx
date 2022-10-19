ed75b42c8fd9a47c112a137965bc929ddaf85a1a
Seraquesada <serafinkocho@gmail.com>
2022-10-19 18:45:57 -0300

    header v2

--- front-end/src/componentes/header/Header.jsx
+++ front-end/src/componentes/header/Header.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import "./Header.module.css";
 
 const Header = () => {
+
+    const [input, setInput] = useState("");
+    const [auto, setAuto] = useState();
+    
+    const handleChange = (e) => {
+        setAuto(e.target.value);
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+    //TODO cambiar logo
   return (
     <header>
-        <nav>
-            <div>
-                <img src="" alt="" srcset="" />
+        <div className="a">
+
+            <div className>            
+                <img id="logo" src="https://archive.org/download/revolt_dev_version/cover.jpg" alt="logo" />
+                <h4>¿Necesitas un auto? Aca te espera</h4>
             </div>
-            <input type="text" placeholder='busca tu auto favorito' />
-            <button>Iniciar Sesion</button>
-            <button>Crear Cuenta</button>
+            <div>
+                <button>Iniciar Sesion</button>
+                <button>Crear Cuenta</button>                
+            </div>
+
+        </div>
+        <nav>
+            <form onSubmit={handleSubmit}>
+                <input type="text" onChange={handleChange} placeholder='busca tu auto favorito' />
+
+                <input type="date" value="check-in"/>
+                <button>Buscar Auto</button>
+            </form>
+
         </nav>
     </header>
   )
