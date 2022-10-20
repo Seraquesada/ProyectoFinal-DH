7f92f2cbe0b65cf0f697b5e8cd2d385b25405f30
Seraquesada <serafinkocho@gmail.com>
2022-10-20 17:43:42 -0300

    todo en orden

--- front-end/src/componentes/footer/Footer.jsx
+++ front-end/src/componentes/footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../Footer.css'
+import './Footer.module.css'
 import {BsFacebook,BsTwitter,BsInstagram} from 'react-icons/bs'
 import {GrLinkedinOption} from 'react-icons/gr'
 
