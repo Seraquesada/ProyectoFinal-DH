76f6a6e5052effe97789c55c182277e14af7db7b
Nicolas Montero Sabelli <nico.monterosabelli@gmail.com>
2022-10-20 19:16:27 -0300

    Se agrego validacion por JWT a la api

--- back-end/ProyectoIntegrador/ProyectoIntegrador/src/main/java/com/Grupo5/ProyectoIntegrador/controller/UsuarioController.java
+++ back-end/ProyectoIntegrador/ProyectoIntegrador/src/main/java/com/Grupo5/ProyectoIntegrador/controller/UsuarioController.java
@@ -1,20 +1,43 @@
 package com.Grupo5.ProyectoIntegrador.controller;
 import com.Grupo5.ProyectoIntegrador.Exceptions.AlreadyExistException;
+import com.Grupo5.ProyectoIntegrador.Security.JWTUtil;
+import com.Grupo5.ProyectoIntegrador.entity.AuthenticationRequest;
+import com.Grupo5.ProyectoIntegrador.entity.AuthenticationResponse;
 import com.Grupo5.ProyectoIntegrador.entity.RolUsuario;
 import com.Grupo5.ProyectoIntegrador.entity.Usuario;
 import com.Grupo5.ProyectoIntegrador.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
     private final UsuarioService usuarioService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JWTUtil jwtUtil;
+
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception{
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+        }catch (BadCredentialsException e) {
+            throw new Exception("Incorrect", e);
+        }
+        final UserDetails userDetails = usuarioService.loadUserByUsername(authenticationRequest.getUsername());
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse((jwt)));
+    }
+
 
     @Autowired
     public UsuarioController(UsuarioService usuarioService) {
