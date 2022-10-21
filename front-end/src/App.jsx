e397304c2866924ba83dfaac9f7976d133965c01
LukaBrc <lucabeati@hotmail.com>
2022-10-21 17:43:14 -0300

    acomodo cositas

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,7 @@
-import './App.css'
-import Header from './componentes/header/Header'
-import Footer from './componentes/footer/Footer'
-import './Footer.css'
+import './App.css';
+import Header from './componentes/header/Header';
+import Footer from './componentes/footer/Footer';
+import Login from './componentes/login/Login';
 
 function App() {
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <div >
       <Header/>
-
+      <Login />
       <Footer/>
     </div>
     
---
+++ front-end/src/componentes/login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Login.module.css";
+
+export const Login = (props) => {
+  return (
+
+    <div>Login</div>
+
+  )
+}
+export default Login
