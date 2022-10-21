2d05976301de155365ee794be27993492a5b776b
Seraquesada <serafinkocho@gmail.com>
2022-10-21 20:14:26 -0300

    header con requisitos

--- front-end/src/componentes/header/Header.jsx
+++ front-end/src/componentes/header/Header.jsx
@@ -1,33 +1,43 @@
 import React,{useState,useEffect} from 'react'
-import "./Header.module.css";
+import Select from 'react-select'
+import "./Header.css";
 
 const Header = () => {
 
-    const [input, setInput] = useState("");
-    const [auto, setAuto] = useState();
-    
-    const handleChange = (e) => {
-        setAuto(e.target.value);
-    }
+
+
     const handleSubmit = (e) => {
         e.preventDefault()
     }
+    const options = [
+        {value: "buenosAires", label: "Buenos Aires"},
+        {value: "laPlata", label: "La Plata"},
+        {value: "comodoroRivadavia", label: "Comodoro Rivadavia"},
+        {value: "salta", label: "Salta"},
+        {value: "bariloche", label: "Bariloche"},
+    ]
     //TODO cambiar logo
   return (
     <header>
         <div className="a">
 
-            <div className>            
+            <div className="right-header">            
                 <img id="logo" src="https://archive.org/download/revolt_dev_version/cover.jpg" alt="logo" />
                 <h4>¿Necesitas un auto? Aca te espera</h4>
+            </div>
+            <div className="left-header">
+                <button>Iniciar Sesion</button>
+                <button>Crear Cuenta</button>
             </div>
         </div>
         <nav>
             <form onSubmit={handleSubmit}>
-                <input type="text" onChange={handleChange} placeholder='busca tu auto favorito' />
-
-                <input type="date" value="check-in"/>
-                <button>Buscar Auto</button>
+                <Select options={options}/>
+                <label htmlFor="check-in">Check-In</label>
+                <input type="date" id='check-in'/>
+                <label htmlFor="check-out">Check-Out</label>
+                <input type="date" id='check-out'/> 
+                
             </form>
 
         </nav>
--- front-end/src/componentes/login/Login.jsx
+++ front-end/src/componentes/login/Login.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import "./Login.module.css";
+import "./Login.css";
 
 
 
