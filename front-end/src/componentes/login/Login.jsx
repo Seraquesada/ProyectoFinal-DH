b4fd3e30a33f13ce3defe6f9f90effe7b0690ee1
LukaBrc <lucabeati@hotmail.com>
2022-10-24 17:53:45 -0300

    validaciones básicas funcionando

--- front-end/src/componentes/login/Login.jsx
+++ front-end/src/componentes/login/Login.jsx
@@ -1,160 +1,160 @@
 import React, {useState} from 'react';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import "./Login.css";
 import axios from "axios";
 
 
 
 export const Login = (props) => {
-    let [authMode, setAuthMode] = useState("signin")
+  const [authMode, setAuthMode] = useState("signin")
+  const [validated, setValidated] = useState(false);
 
-    const changeAuthMode = () => {
-      setAuthMode(authMode === "signin" ? "signup" : "signin")
-    }
-    const iniciarSesion = (e) => {
-      e.preventDefault();
-      let payload = {
-        "username": document.querySelector("#email").value,
-        "password" : document.querySelector("#contraseña").value
-      }
-      axios.post('http://localhost:8080/usuarios/authenticate', payload)
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      setValidated(true);
+    }else {
+      registrarUsuario(event)
     }
 
-    const registrarUsuario = (e) => {
-      e.preventDefault();
-      let payload = {
-        "nombre": document.querySelector("#nombre").value,
-        "apellido": document.querySelector("#apellido").value,
-        "email": document.querySelector("#email").value,
-        "password" : document.querySelector("#contraseña").value
-      }
-      axios.post('http://localhost:8080/usuarios/register', payload)
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+
+  };
+
+  const changeAuthMode = () => {
+    setAuthMode(authMode === "signin" ? "signup" : "signin")
+  }
+  const iniciarSesion = (e) => {
+    e.preventDefault();
+    let payload = {
+      "username": document.querySelector("#email").value,
+      "password" : document.querySelector("#contraseña").value
     }
-  
-    if (authMode === "signin") {
-      return (
-        <div className="Auth-form-container bg-dark">
-          <form className="Auth-form">
-            <div className="Auth-form-content">
-              <h3 className="Auth-form-title">Iniciar Sesión</h3>
-              <div className="form-floating mt-3">
-                <input
-                  type="email"
-                  className="form-control mt-1"
-                  id="email"
-                  placeholder="Ingresa tu correo electrónico"
-                />
-                <label htmlFor="email">Correo Electrónico</label>
-              </div>
-              <div className="form-floating mt-3">
-                <input
-                  type="password"
-                  className="form-control mt-1"
-                  id="password"
-                  placeholder="Ingresa tu contraseña"
-                />
-                <label htmlFor="password">Contraseña</label>
-              </div>
-              <div className="d-grid gap-2 mt-3">
-                <button type="submit" className="btn btn-warning" onClick={iniciarSesion}>
-                  Ingresar
-                </button>
-              </div>
-              <div className="text-center">
-              ¿Todavía no te registraste?{" "}
-                <span className="link-primary" onClick={changeAuthMode}>
-                  Registrarse
-                </span>
-              </div>
-              <p className="text-center mt-2">
-              ¿Olvidaste tu <a href="#">contraseña</a>?
-              </p>
-            </div>
-          </form>
-        </div>
-      )
+    axios.post('http://localhost:8080/usuarios/authenticate', payload)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  const registrarUsuario = (e) => {
+    e.preventDefault();
+    let payload = {
+      "nombre": document.querySelector("#nombre").value + " " + document.querySelector("#apellido").value,
+      "userName": document.querySelector("#email").value,
+      "email": document.querySelector("#email").value,
+      "password" : document.querySelector("#contraseña").value
     }
+    axios.post('http://localhost:8080/usuarios', payload)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
   
+  if (authMode === "signin") {
     return (
       <div className="Auth-form-container bg-dark">
         <form className="Auth-form">
           <div className="Auth-form-content">
-            <h3 className="Auth-form-title">Crear Cuenta</h3>
-            <div className="form-floating mt-3">
-              <input
-                type="text"
-                id="nombre"
-                className="form-control mt-1"
-                placeholder="Nombre"
-              />
-              <label htmlFor="nombre">Nombre</label>
-            </div>
-            <div className="form-floating mt-3">
-              <input
-                type="text"
-                id="apellido"
-                className="form-control mt-1"
-                placeholder="Apellido"
-              />
-              <label htmlFor="apellido">Apellido</label>
-            </div>
+            <h3 className="Auth-form-title">Iniciar Sesión</h3>
             <div className="form-floating mt-3">
               <input
                 type="email"
-                id="email"
                 className="form-control mt-1"
-                placeholder="ejemplo@mail.com"
-                required
+                id="email"
+                placeholder="Ingresa tu correo electrónico"
               />
-              <label htmlFor="email">Correo Elecrónico</label>
+              <label htmlFor="email">Correo Electrónico</label>
             </div>
             <div className="form-floating mt-3">
               <input
                 type="password"
-                id="password"
                 className="form-control mt-1"
+                id="password"
                 placeholder="Ingresa tu contraseña"
-                required
-                minLength={6}
               />
               <label htmlFor="password">Contraseña</label>
             </div>
-            <div className="form-floating mt-3">
-              <input
-                type="password"
-                id="passwordCheck"
-                className="form-control mt-1"
-                placeholder="Confirmar Contraseña"
-                required
-                minLength={6}
-              />
-              <label htmlFor="passwordCheck">Confirmar Contraseña</label>
-            </div>
             <div className="d-grid gap-2 mt-3">
-              <button type="submit" className="btn btn-warning" onClick={registrarUsuario}>
-                Registrarse
+              <button type="submit" className="btn btn-warning" onClick={iniciarSesion}>
+                Ingresar
               </button>
             </div>
             <div className="text-center">
+            ¿Todavía no te registraste?{" "}
+              <span className="link-primary" onClick={changeAuthMode}>
+                Registrarse
+              </span>
+            </div>
+            <p className="text-center mt-2">
+            ¿Olvidaste tu <a href="#">contraseña</a>?
+            </p>
+          </div>
+        </form>
+      </div>
+    ) 
+  }
+
+  return (
+    <div className="Auth-form-container bg-dark">
+      <Form className="Auth-form" noValidate validated={validated} onSubmit={handleSubmit}>
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Iniciar Sesión</h3>
+            <Form.Group className="mb-3" controlId="nombre">
+              <FloatingLabel controlId="nombre" label="Nombre" className="mb-3">
+                <Form.Control type="text" placeholder="Nombre" required/>
+              </FloatingLabel>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="apellido">
+              <FloatingLabel controlId="apellido" label="Apellido" className="mb-3">
+                <Form.Control type="text" placeholder="Apellido" required/>
+              </FloatingLabel>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="email">
+              <FloatingLabel controlId="email" label="Correo Elecrónico" className="mb-3">
+                <Form.Control type="email" placeholder="correo@dominio.com" required/>
+              </FloatingLabel>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="password">
+              <FloatingLabel controlId="password" label="Contraseña">
+                <Form.Control type="password" placeholder="Contraseña" required/>
+              </FloatingLabel>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="passwordCheck">
+              <FloatingLabel controlId="passwordCheck" label="Verificar Contraseña">
+                <Form.Control type="password" placeholder="passwordCheck" min={6} required />
+              </FloatingLabel>
+            </Form.Group>
+            <Form.Group className="mb-3">
+            <Form.Check
+              required
+              label="Acepto términos y condiciones."
+              feedback="Debes aceptar los términos y condiciones antes de registrarte"
+              feedbackType="invalid"
+            />
+            </Form.Group>
+            <div className="d-grid gap-2 mt-3">
+              <Button type="submit" className="btn-warning">
+                Crear Cuenta
+              </Button>
+              <div className="text-center">
               ¿Ya estás registrado?{" "}
               <span className="link-primary" onClick={changeAuthMode}>
                 Iniciar Sesión
               </span>
+              </div>
             </div>
-          </div>
-        </form>
-      </div>
-    )
+        </div>
+      </Form>
+    </div>    
+  )
 }
 export default Login
