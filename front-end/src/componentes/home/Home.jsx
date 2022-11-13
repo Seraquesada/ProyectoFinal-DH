63f7c86c6a83e6be5a47c1d0dd40180e39f0d437
LukaBrc <lucabeati@hotmail.com>
2022-11-13 20:55:55 -0300

    importo bien GroupOfVehicles

--- front-end/src/componentes/home/Home.jsx
+++ front-end/src/componentes/home/Home.jsx
@@ -2,8 +2,7 @@ import React  from "react";
 import Header from "../header/Header";
 import Category from "../category/Category"
 import Footer from "../footer/Footer"
-import GroupOfVehicles from "../GroupOfVehicles/GroupOfVehicles";
-
+import GroupOfVehicles from "../groupOfVehicles/GroupOfVehicles";
 import SearchBar from "../searchBar/SearchBar"
 import { CategoryProvider } from "../../context/CategoryContext";
 
