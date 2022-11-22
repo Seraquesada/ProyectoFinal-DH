daf1eb1860f31fdd2bc76ac8d9355f1527f1ce26
Seraquesada <serafinkocho@gmail.com>
2022-11-22 13:44:50 -0300

    boton para limpiar filtros

--- front-end/src/hooks/useDateChange.jsx
+++ front-end/src/hooks/useDateChange.jsx
@@ -1,35 +1,15 @@
-import { useContext,useEffect } from 'react';
-
+import { useContext } from 'react';
 import { DateContext } from '../context/DateContext';
 
 export const useDateChange = () => {
-    
+
     const {startDate, setStartDate , endDate, setEndDate} = useContext(DateContext)
-    
+
     const handleDateChange = (dates) => {
         const [start, end] = dates;
         setStartDate(start);
         setEndDate(end);
     };
-
-    useEffect(() => {
-        window.localStorage.setItem('startDate', startDate.toISOString().split("T")[0]);
-    }, [startDate]);
-
-    useEffect(() => {
-        if(endDate !== null) {
-            window.localStorage.setItem('endDate', endDate.toISOString().split("T")[0]);
-        }
-    },[endDate])
-
-    useEffect(() => {
-        //setEndDate((window.localStorage.getItem('endDate')));
-            
-    }, [endDate]);
-    
-    useEffect(() => {
-        //setStartDate(JSON.parse(window.localStorage.getItem('startDate')));
-    }, []);
 
     return {startDate, endDate, handleDateChange};
 }
