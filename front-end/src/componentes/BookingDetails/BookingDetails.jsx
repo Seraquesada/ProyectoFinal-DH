b81c51aab99e2633b1ba8a962bf2133af2b3f3c0
Seraquesada <serafinkocho@gmail.com>
2022-11-24 12:04:42 -0300

    fecha en booking details

--- front-end/src/componentes/BookingDetails/BookingDetails.jsx
+++ front-end/src/componentes/BookingDetails/BookingDetails.jsx
@@ -5,8 +5,10 @@ import './BookingDetails.css'
 
 const BookingDetails = ({respuesta}) => {
 
-    const {startDate, setStartDate , endDate, setEndDate} = useContext(DateContext)
+    const {startDate, setStartDate , endDate, setEndDate,checkIn,setCheckIn,checkOut,setCheckOut} = useContext(DateContext)
 
+    const startNormalized = startDate.toISOString().split("T")[0].split("-").join("/")
+    const endNormalized = endDate?.toISOString().split("T")[0].split("-").join("/")
     return(
         <div className="infoCard">
             <h3> Detalle de Reserva</h3>
@@ -15,9 +17,9 @@ const BookingDetails = ({respuesta}) => {
             <h4> {respuesta.titulo}</h4>
             <h5> {respuesta.ciudad.nombre}</h5>
             <hr></hr>
-            <h4 className='checkinOut'> Check in  </h4>
+            <h4 className='checkinOut'> Check in {startNormalized} </h4>
             <hr></hr>
-            <h4 className='checkinOut'> Check out </h4>
+            <h4 className='checkinOut'> Check out {endNormalized} </h4>
             <button className='btn btn-primary'>Confirmar reserva</button>  
         </div>
     );
