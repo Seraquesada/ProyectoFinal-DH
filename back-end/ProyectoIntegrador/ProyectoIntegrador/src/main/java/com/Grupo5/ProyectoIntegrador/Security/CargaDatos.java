957d5eea05b4e66bf58f3e94d0f853fe70d285e5
LukaBrc <lucabeati@hotmail.com>
2022-12-13 15:20:50 -0300

    DatosComentados.

--- back-end/ProyectoIntegrador/ProyectoIntegrador/src/main/java/com/Grupo5/ProyectoIntegrador/Security/CargaDatos.java
+++ back-end/ProyectoIntegrador/ProyectoIntegrador/src/main/java/com/Grupo5/ProyectoIntegrador/Security/CargaDatos.java
@@ -32,7 +32,7 @@ public class CargaDatos implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        /*BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String password = "digital";
         String passHash = passwordEncoder.encode(password);
         Usuario usuario = new Usuario();
@@ -280,7 +280,7 @@ public class CargaDatos implements ApplicationRunner {
         productoService.guardar(producto4);
         productoService.guardar(producto5);
         productoService.guardar(producto6);
-        productoService.guardar(producto7);
+        productoService.guardar(producto7);*/
 
         /*Categoria categoria1 = new Categoria("Sedan", "Vehiculo de cuatro puertas y baúl. Ideal para cuatro personas y dos valijas grandes.", "https://images.unsplash.com/photo-1546614042-7df3c24c9e5d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80");
 
@@ -294,7 +294,7 @@ public class CargaDatos implements ApplicationRunner {
         LocalDate fechaInicio2 = LocalDate.of(2022, 12, 10);
         LocalDate fechaFin2 = LocalDate.of(2022, 12, 21);
         Reserva reserva2 = new Reserva(time2, fechaInicio2, fechaFin2, productoService.buscar(4L), usuario);
-        reservaservice.guardar(reserva2);*/
+        reservaservice.guardar(reserva2);/*
 
         /*Categoria categoria1 = new Categoria("Sedan", "Vehiculo de cuatro puertas y baúl. Ideal para cuatro personas y dos valijas grandes.", "https://images.unsplash.com/photo-1546614042-7df3c24c9e5d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80");
 
